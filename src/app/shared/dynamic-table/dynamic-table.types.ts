7edcb656530577a1205067239abff2d039ddae9e
Ibrahim Al-herby <ibrahim.fullstack.dev@gmail.com>
2026-07-19 08:29:40 +0300

    chore(dashboard): Delete src/app/features/dashboard/components/dashboard-tables & src/app/shared/dynamic-table.

--- src/app/shared/dynamic-table/dynamic-table.types.ts
+++
@@ -1,5 +0,0 @@
-export interface TableColumn {
-  key: string; // المفتاح المطابق للحقل في كائن البيانات (مثل: 'amount')
-  label: string; // العنوان الذي سيظهر في أعلى العمود (مثل: 'المبلغ' أو 'Amount')
-  type?: 'text' | 'currency' | 'date' | 'badge'; // نوع البيانات لتحديد طريقة المعالجة والتنسيق
-}
